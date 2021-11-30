a9f6d1041d5b75db21d21782a51c1ced6b110e37
Luis V. Capelletto <thecapellett@gmail.com>
2021-11-30 17:07:06 -0300

    create user

---
+++ challenges/desafio_01_conceitos/README.md
@@ -0,0 +1,16 @@
+## Sobre o Desafio
+
+Essa será uma aplicação para gerenciar tarefas (em inglês todos). 
+Será permitida a criação de um usuário com name e username, bem como fazer o CRUD de todos:
+
+- [x] Criar um novo _todo_;
+- [ ] Listar todos os _todos_;
+- [ ] Alterar o `title` e `deadline` de um _todo_ existente;
+- [ ] Marcar um _todo_ como feito;
+- [ ] Excluir um _todo_;
+
+Tudo isso para cada usuário em específico (o `username` será passado pelo header).
+
+### Regras de Negócio
+
+- [x] Não deve ser possível criar um usuário com um `username` já existente
--- challenges/desafio_01_conceitos/src/index.js
+++ challenges/desafio_01_conceitos/src/index.js
@@ -1,21 +1,39 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { randomUUID } = require('crypto');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
   // Complete aqui
+
+
+
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const { name, username } = request.body;
+
+  const userAlreadyExists = users.find((user) => user.username === username);
+
+  if (userAlreadyExists) {
+    return response.status(400).json({ error: 'User already exists' });
+  }
+
+  users.push({
+    name,
+    username,
+    todos: [],
+    uid: randomUUID(),
+  })
+
+  response.status(201).send();
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
