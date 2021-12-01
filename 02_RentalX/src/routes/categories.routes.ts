3d75c57a51bbf559dc28decfc0a249a2ddbad8d7
Luis V. Capelletto <thecapellett@gmail.com>
2021-11-30 23:02:55 -0300

    feat: create category

---
+++ 02_RentalX/src/routes/categories.routes.ts
@@ -0,0 +1,23 @@
+import { Router } from "express";
+import { randomUUID } from "crypto";
+import { Category } from "../model/Category";
+
+const categoriesRoutes = Router();
+
+const categories: Category[] = [];
+
+categoriesRoutes.post("/", (req, res) => {
+  const { name, description } = req.body;
+  const newCategory = new Category();
+
+  Object.assign(newCategory, {
+    name,
+    description,
+    created_at: new Date(),
+  });
+
+  categories.push(newCategory);
+  return res.status(201).send(newCategory);
+});
+
+export { categoriesRoutes };
