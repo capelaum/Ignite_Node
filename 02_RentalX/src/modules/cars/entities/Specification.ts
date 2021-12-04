428ceb26e3944503cdf3c10ccf8625ec0f2e1081
Luis V. Capelletto <thecapellett@gmail.com>
2021-12-04 06:26:55 -0300

    feat: SpecificationsRepository

--- 02_RentalX/src/modules/cars/entities/Specification.ts
+++ 02_RentalX/src/modules/cars/entities/Specification.ts
@@ -1,9 +1,18 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm";
 import { randomUUID } from "crypto";
 
+@Entity("specifications")
 class Specification {
+  @PrimaryColumn()
   id?: string;
+
+  @Column()
   name: string;
+
+  @Column()
   description: string;
+
+  @CreateDateColumn()
   created_at: Date;
 
   constructor() {
