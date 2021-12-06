7a0189fe99adc35d3c5ab8976ca1bf56bed3f9e9
Luis V. Capelletto <thecapellett@gmail.com>
2021-12-06 04:26:32 -0300

    feat: hash User password

--- 02_RentX/src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ 02_RentX/src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -1,4 +1,5 @@
 import { inject, injectable } from "tsyringe";
+import { hash } from "bcrypt";
 
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
@@ -16,11 +17,19 @@ class CreateUserUseCase {
     driver_license,
     password,
   }: ICreateUserDTO): Promise<void> {
+    const userAlreadyExists = await this.usersRepository.findByEmail(email);
+
+    if (userAlreadyExists) {
+      throw new Error("User already exists");
+    }
+
+    const hashedPassword = await hash(password, 8);
+
     await this.usersRepository.create({
       name,
       email,
       driver_license,
-      password,
+      password: hashedPassword,
     });
   }
 }
