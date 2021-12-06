3761623063bf47d3eccd3868bb40bd65fb503765
Luis V. Capelletto <thecapellett@gmail.com>
2021-12-06 03:58:50 -0300

    feat: UsersRepository

---
+++ 02_RentX/src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -0,0 +1,29 @@
+import { inject } from "tsyringe";
+
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
+
+class CreateUserUseCase {
+  constructor(
+    @inject("UsersRepository")
+    private usersRepository: IUsersRepository
+  ) {}
+
+  async execute({
+    name,
+    username,
+    email,
+    driver_license,
+    password,
+  }: ICreateUserDTO): Promise<void> {
+    await this.usersRepository.create({
+      name,
+      username,
+      email,
+      driver_license,
+      password,
+    });
+  }
+}
+
+export { CreateUserUseCase };
