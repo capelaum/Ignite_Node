780f5f4f8b7fca3b9897e766b68cb554bd3b4e7b
Luis V. Capelletto <thecapellett@gmail.com>
2021-12-06 03:40:06 -0300

    migration: CreateUsers

---
+++ 02_RentX/src/modules/accounts/entities/User.ts
@@ -0,0 +1,37 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm";
+import { randomUUID } from "crypto";
+
+@Entity("users")
+class User {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  username: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  driver_license: string;
+
+  @Column()
+  isAdmin: boolean;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = randomUUID();
+    }
+  }
+}
+
+export { User };
