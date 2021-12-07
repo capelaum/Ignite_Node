b2970a0c7967976beb90c9694a49c0708d506278
Luis V. Capelletto <thecapellett@gmail.com>
2021-12-07 16:12:59 -0300

    refactor: app architecture

--- 02_RentX/src/shared/infra/http/server.ts
+++ 02_RentX/src/shared/infra/http/server.ts
@@ -3,13 +3,13 @@ import "reflect-metadata";
 import express, { NextFunction, Request, Response } from "express";
 import swaggerUi from "swagger-ui-express";
 
-import "./database";
+import "@shared/infra/typeorm";
 
 import "@shared/container";
-import { AppError } from "@errors/AppError";
+import { AppError } from "@shared/errors/AppError";
 
+import swaggerFile from "../../../swagger.json";
 import { router } from "./routes";
-import swaggerFile from "./swagger.json";
 
 const app = express();
 app.use(express.json());
