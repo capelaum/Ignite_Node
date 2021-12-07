d46d86634b777c011c3a6a2f1413a188901074a2
Luis V. Capelletto <thecapellett@gmail.com>
2021-12-07 13:05:45 -0300

    test: Authenticate User use case

---
+++ 02_RentX/src/modules/accounts/repositories/in-memory/UsersRepositoryInMemory.ts
@@ -0,0 +1,35 @@
+import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
+import { User } from "../../entities/User";
+import { IUsersRepository } from "../IUsersRepository";
+
+class UsersRepositoryInMemory implements IUsersRepository {
+  users: User[] = [];
+
+  async create({
+    name,
+    email,
+    driver_license,
+    password,
+  }: ICreateUserDTO): Promise<void> {
+    const newUser = new User();
+
+    Object.assign(newUser, {
+      name,
+      email,
+      driver_license,
+      password,
+    });
+
+    this.users.push(newUser);
+  }
+
+  async findByEmail(email: string): Promise<User> {
+    return this.users.find((user) => user.email === email);
+  }
+
+  async findById(id: string): Promise<User> {
+    return this.users.find((user) => user.id === id);
+  }
+}
+
+export { UsersRepositoryInMemory };
